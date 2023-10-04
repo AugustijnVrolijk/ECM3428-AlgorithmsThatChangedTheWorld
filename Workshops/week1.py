4ee3eaedcb63878badffb65ba00050f71111e8a2
augustijn <av407@exeter.ac.uk>
2023-10-04 16:31:42 +0100

    transition matrix for pagerank added

--- Workshops/week1.py
+++ Workshops/week1.py
@@ -1,23 +1,41 @@
 import numpy as np
 
 class webPage():
-    def __init__(self, selfURL: str, linkURLs: list=[]):
+    def __init__(self, selfURL: int, linkURLs: list=[]):
         self.selfURL = selfURL
         self.linkURLs = linkURLs
-        self.rank = 1/len(self.linkURLs)
-    
+        if len(self.linkURLs) != 0:
+            self.rank = 1/len(self.linkURLs)
+        else:
+            self.rank = 0
+            
 def calculateRank():
     return
 
+def calculateTransitionMatrix(links:list):
+    #transitionMatrix (TM) is a modifier to the pang ranks based on how many links a page has.
+    TM = []
+    for i in range(len(links)):
+        TM.append([0]*len(links))
+
+    for page in links:
+        for link in page.linkURLs:
+            TM[page.selfURL][link.selfURL] = page.rank
+    return TM 
+
+def setup():
+    page1 = webPage(1)
+    page0 = webPage(0, [page1])
+    page3 = webPage(3, [page0])
+    page4 = webPage(4, [page3])
+    page2 = webPage(2, [page1, page4])
+    links = [page0,page1,page2,page3,page4]
+    return links
 
 def main():
-    links = []
-    size = 5
-    page2 = webPage("2")
-    page1 = webPage("1", [page2])
-    page4 = webPage("4", [page1])
-    page5 = webPage("5", [page4])
-    page3 = webPage("3", [page2, page5])
+    links = setup()
+    TM = calculateTransitionMatrix(links)
+    print(TM)
     return
 
 
