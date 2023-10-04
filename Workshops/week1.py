08a6c15a9b7153e763ef5163d8e0aa585a2ef2cb
augustijn <av407@exeter.ac.uk>
2023-10-04 15:34:07 +0100

    Create web directed graph

--- Workshops/week1.py
+++ Workshops/week1.py
@@ -1,16 +1,23 @@
+import numpy as np
+
 class webPage():
-    def __init__(self, selfURL: str, linkURLs: list):
+    def __init__(self, selfURL: str, linkURLs: list=[]):
         self.selfURL = selfURL
         self.linkURLs = linkURLs
+        self.rank = 1/len(self.linkURLs)
     
-
-
-
-
-
+def calculateRank():
+    return
 
 
 def main():
+    links = []
+    size = 5
+    page2 = webPage("2")
+    page1 = webPage("1", [page2])
+    page4 = webPage("4", [page1])
+    page5 = webPage("5", [page4])
+    page3 = webPage("3", [page2, page5])
     return
 
 
